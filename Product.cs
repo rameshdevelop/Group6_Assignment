aaad03d10fddc4e3573bef1f0ccfbaa48cad2ecb
Ramesh Selvam <156361443+rameshdevelop@users.noreply.github.com>
2024-06-04 18:52:31 -0400

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group6_Assignment
+{
+    internal class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productId, string productName, double price, int stock)
+        {
+
+            ProductID = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncrementStock(int value)
+        {
+
+            Stock += value;
+        }
+
+        public void DecrementStock(int value)
+        {
+
+            if (Stock >= value)
+            {
+                Stock -= value;
+            }
+            else
+            {
+                Console.WriteLine("Not enough stock to decrease.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group6_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(100, "External Hard Drive", 79.99, 70);
+
+            bool continueOperation = true;
+
+            while (continueOperation)
+            {
+                Console.WriteLine("Choose an operation:");
+                Console.WriteLine("1. Increment stock");
+                Console.WriteLine("2. Decrement stock");
+                Console.WriteLine("3. Close");
+                Console.WriteLine("\n");
+
+                string operationChoice = FetchStringFromConsole("Choose Your option:");
+
+                switch (operationChoice)
+                {
+                    case "1":
+                        AddToStock(product);
+                        break;
+                    case "2":
+                        ReduceStock(product);
+                        break;
+                    case "3":
+                        continueOperation = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void AddToStock(Product product)
+        {
+            int increaseAmount = FetchIntegerFromConsole("Enter the quantity to increase the stock by:");
+            product.IncrementStock(increaseAmount);
+            Console.WriteLine($"Stock increased by {increaseAmount}. New stock: {product.Stock} \n");
+        }
+
+        static void ReduceStock(Product product)
+        {
+            int decreaseAmount = FetchIntegerFromConsole("Enter the quantity to decrease the stock by:");
+            product.DecrementStock(decreaseAmount);
+            Console.WriteLine($"Stock decreased by {decreaseAmount}. New stock: {product.Stock} \n");
+        }
+
+        static string FetchStringFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine()?.Trim() ?? "";
+
+            while (String.IsNullOrEmpty(userInput))
+            {
+                userInput = Console.ReadLine()?.Trim() ?? "";
+            }
+            return userInput;
+        }
+
+        static int FetchIntegerFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            int userInput;
+            while (!int.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("Please Enter a valid input.");
+            }
+            return userInput;
+        }
+    }
+}
